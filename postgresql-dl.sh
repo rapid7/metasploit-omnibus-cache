e21cc04a31d9d258fb7e955fda550714559fdc12
Brent Cook <busterb@gmail.com>
2017-02-19 12:38:40 -0600

    grab new postgresql 9.6.2

---
+++ postgresql-dl.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+version=9.6.2
+if [ ! -f postgresql-$version.tar.bz2 ]; then
+  curl -O https://ftp.postgresql.org/pub/source/v$version/postgresql-$version.tar.bz2
+else
+  echo postgresql-$version.tar.bz2 exists
+fi
+sha256sum postgresql-$version.tar.bz2
+
+if [ ! -f postgresql-$version-1-windows-binaries.zip ]; then
+	curl -O http://get.enterprisedb.com/postgresql/postgresql-$version-1-windows-binaries.zip
+	rm -fr pgsql
+	unzip postgresql-$version-1-windows-binaries.zip
+	rm -fr pgsql/doc pgsql/pgAdmin\ 4 pgsql/StackBuilder pgsql/symbols
+	rm postgresql-$version-1-windows-binaries.zip
+	zip -r postgresql-$version-1-windows-binaries.zip pgsql
+	rm -fr pgsql
+else
+	echo postgresql-$version-1-windows-binaries.zip exists
+fi
+sha256sum postgresql-$version-1-windows-binaries.zip
